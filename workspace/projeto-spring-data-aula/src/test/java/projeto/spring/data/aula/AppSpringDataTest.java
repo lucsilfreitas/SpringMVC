522bdeaeb3c57942599fbcd55242a8ef657d3c4e
Luciano Freitas <lucsilfreitas@gmail.com>
2020-09-14 19:22:15 -0300

    xml erro

---
+++ workspace/projeto-spring-data-aula/src/test/java/projeto/spring/data/aula/AppSpringDataTest.java
@@ -0,0 +1,15 @@
+package projeto.spring.data.aula;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations  = {"classpath:META-INF/spring-config.xml"})
+public class AppSpringDataTest {
+	
+	public void testeInsert() {
+		System.out.println("Iniciou spring com sucesso!");
+	}
+
+}
